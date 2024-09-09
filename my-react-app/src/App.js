5302eeb9cfec9626ad717bcd969771c5e8800a1a
VarunRavi95 <58401608+VarunRavi95@users.noreply.github.com>
2024-09-09 11:51:23 +0000

    Commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
