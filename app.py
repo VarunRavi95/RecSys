ea19c1491bd7b5233a151053cab84bf4104d110a
VarunRavi95 <58401608+VarunRavi95@users.noreply.github.com>
2024-09-10 13:42:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
 
-# Set up CORS to allow requests from your React app
-CORS(app, resources={r"/recommend": {"origins": "http://127.0.0.1:5000/recommend"}}, supports_credentials=True)
+# Set up CORS to allow requests from any origin
+CORS(app, resources={r"/recommend": {"origins": "*"}}, supports_credentials=True)
 
 @app.after_request
 def after_request(response):
@@ -20,8 +20,8 @@ def after_request(response):
 
 # Load data and model
 df = pd.read_csv('processed_listings_with_original_descriptions.csv')
-embeddings = np.load('listings_embeddings.npy').astype('float32')
-model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+embeddings = np.load('combined_embeddings.npy').astype('float32')
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 # Set up FAISS index
 index = faiss.IndexFlatL2(embeddings.shape[1])
@@ -37,8 +37,17 @@ def recommend():
     data = request.json
     query = data.get('query')
     query_embedding = model.encode([query]).astype('float32')
+
+    # Ensure query embedding matches the dimension of index embeddings
+    if query_embedding.shape[1] < embeddings.shape[1]:
+        query_embedding = np.pad(query_embedding, ((0, 0), (0, embeddings.shape[1] - query_embedding.shape[1])), 'constant')
+    elif query_embedding.shape[1] > embeddings.shape[1]:
+        query_embedding = query_embedding[:, :embeddings.shape[1]]
+
+    # Perform the search using FAISS
     distances, indices = index.search(query_embedding, 20)
-    # Handle NaN values in the DataFrame by converting them to None (which will be converted to null in JSON)
+
+    # Handle NaN values in the DataFrame by converting them to None
     recommendations = df.iloc[indices[0]].replace({np.nan: None}).to_dict(orient='records')
     return jsonify(recommendations)
 
